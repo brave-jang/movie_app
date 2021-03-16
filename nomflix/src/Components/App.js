0cf8ac4f3acbb0d743c854d2601cd87b418e6f6b
bravejang <wkdrmfmt@gmail.com>
2021-03-16 09:58:53 +0900

    React Router

---
+++ nomflix/src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Router from "./Router";
+import Header from "./Header";
+
+function App() {
+  return (
+    <>
+      <Router />
+      <Header />
+    </>
+  );
+}
+
+export default App;
