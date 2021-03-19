1afd389580b920e39664474b58d9c7f9e489c513
bravejang <wkdrmfmt@gmail.com>
2021-03-19 15:06:47 +0900

    TVPresenter

--- nomflix/src/Routes/Home/HomePresenter.js
+++ nomflix/src/Routes/Home/HomePresenter.js
@@ -2,27 +2,36 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "Components/Section";
+import Loader from "../../Components/Loader";
 
 const Container = styled.div`
-  padding: 0px 10px;
+  padding: 0px 20px;
 `;
 
 const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
-  loading ? null : (
+  loading ? (
+    <Loader />
+  ) : (
     <Container>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="상영 중인 컨텐츠">
-          {nowPlaying.map((movie) => movie.title)}
+          {nowPlaying.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
         </Section>
       )}
       {upcoming && nowPlaying.length > 0 && (
         <Section title="상영 예정 컨텐츠">
-          {upcoming.map((movie) => movie.title)}
+          {upcoming.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
         </Section>
       )}
       {popular && nowPlaying.length > 0 && (
         <Section title="인기 있는 컨텐츠">
-          {popular.map((movie) => movie.title)}
+          {popular.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
         </Section>
       )}
     </Container>
--- nomflix/src/Routes/TV/TVPresenter.js
+++ nomflix/src/Routes/TV/TVPresenter.js
@@ -1,9 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "../../Components/Section";
+import Loader from "../../Components/Loader";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
 
 const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
-  null;
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="상영 중인 컨텐츠">
+          {topRated.map((show) => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="상영 예정 컨텐츠">
+          {popular.map((show) => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="인기 있는 컨텐츠">
+          {airingToday.map((show) => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+    </Container>
+  );
 
 TVPresenter.propTypes = {
   topRated: PropTypes.array,
